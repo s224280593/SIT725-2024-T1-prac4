89d0f7c5a2e022d9379e6735a59c5987968974e8
s224280593 <162409765+s224280593@users.noreply.github.com>
2024-05-17 18:50:16 +1000

    my first commit

--- server.js
+++ server.js
@@ -1,21 +1,68 @@
-let express=require('express');
-let app= express();
-let port=3000;
-app.use(express.static(__dirname +'/'));
-app.get('/',(req,res)=>{
-    res.render('index.html');
-});
-app.get('/addtwonumbers',(req,res) => {
-    //1 gran the values from url parameter
-    let value1=req.query.num1;
-    let value2=req.query.num2;
-    // 2 calculation
-    let result=parseInt(value1)+parseInt(value2);
-    // 3 return the response, the response need to be in object
-    let response={data:result,statuscode:200,message:'success'}
-    res.json(response);
-});
-app.listen(port,()=>{
-   console.log('server started!');
+let express = require('express');
+let app = express();
+let port = process.env.port || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ThoufiyaShaik:24112003@cluster0.vnmbusa.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
 });
 
+async function run() {
+    try {
+      await client.connect();
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      await client.close();
+    }
+}
+
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.post('/api/place', async (req, res) => {
+    let place = req.body;
+    let result = await postplace(place);
+    client.close();
+    res.json({statusCode: 201, message: 'success', data: result});
+});
+
+async function postplace(place) {
+    await client.connect();
+    let collection = await client.db().collection('places');
+    return collection.insertOne(place);
+}
+
+app.get('/api/places', async (req, res) => {
+    let result = await getAllplaces();
+    client.close();
+    res.json({statusCode: 201, message: 'success', data: result});
+});
+
+async function getAllplaces() {
+    await client.connect();
+    let collection = await client.db().collection('places');
+    return collection.find().toArray();
+}
+
+app.listen(port, () => {
+    console.log('server started');
+    //run().catch(console.dir);;
+});
+// // Define a GET endpoint to server the card data
+// app.get('/api/places',(req,res)=> {
+//     req.json({ cards:cardList});
+// });
+// app.get('/api/places',(req,res)=> {
+//     res.json({statuscode:200, data: cardList, message:"success"})
+// })
